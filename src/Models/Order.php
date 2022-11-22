e625f8d3cc29a7a71b5f43bd6a56f8c7dfee5da6
Mohamed Gamal <mg.dev1992@gmail.com>
2022-11-22 22:08:20 +0200

    add Order & OrderItem models

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -2,12 +2,23 @@
 
 namespace MG\Order\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model as ModelAlias;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @method static find(int $int)
  */
-class Order extends Model
+class Order extends ModelAlias
 {
+    protected $fillable = [
+        'id',
+        'user_id',
+        'total',
+        'status'
+    ];
 
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- src/Models/OrderItem.php
+++ src/Models/OrderItem.php
@@ -3,11 +3,24 @@
 namespace MG\Order\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * @method static find(int $int)
  */
 class OrderItem extends Model
 {
+    protected $fillable = [
+        'id',
+        'order_id',
+        'item_type',
+        'item_id',
+        'quantity',
+    ];
 
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
